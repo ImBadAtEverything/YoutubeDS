c681f227de82eb73f2522d4caa5344784898ed12
ImBadAtEverything <88509307+ImBadAtEverything@users.noreply.github.com>
2021-08-05 13:47:09 -0500

    Update ythttps.php

--- https_removal/ythttps.php
+++ https_removal/ythttps.php
@@ -3,7 +3,7 @@ $url = $_GET['u'];
 $parts = parse_url($url);
 parse_str($parts['query'], $query);
 $key = $query['key'];
-if($key == "YOUR API KEY HERE")
+if($key == "=AIzaSyDySWgifr2yJ-0G8tFuZ9KfSfIkGlG1S_o")
 {
 	echo file_get_contents("$url");
 }
